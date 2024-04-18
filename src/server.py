61b8b373dd69f3227c2177dc9beabcd3676a9b69
Zach <147861361+1054009@users.noreply.github.com>
2024-04-18 14:58:37 -0400

    Choose database to use

--- src/server.py
+++ src/server.py
@@ -8,16 +8,17 @@ FREEDB_USERNAME = "freedb_1054009"
 FREEDB_PASSWORD = "$hchYjtYar$BAh2" # pls don't hack kthx
 FREEDB_HOST = "sql.freedb.tech"
 FREEDB_PORT = 3306
+FREEDB_DB = "freedb_cset180final"
 
 # Setup Flask and connect to the database
 app = Flask(__name__)
-engine = create_engine(f"mysql://{FREEDB_USERNAME}:{FREEDB_PASSWORD}@{FREEDB_HOST}:{FREEDB_PORT}")
+engine = create_engine(f"mysql://{FREEDB_USERNAME}:{FREEDB_PASSWORD}@{FREEDB_HOST}:{FREEDB_PORT}/{FREEDB_DB}")
 sql = engine.connect()
 
 # Routes
 @app.route("/")
 def test():
-    return render_template("base.html")
+	return render_template("base.html")
 
 # Brap you
 if platform.system() == "Windows" and __name__ == "__main__":
