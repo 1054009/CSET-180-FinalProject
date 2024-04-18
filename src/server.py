fd1168b450a084ed82651a5b601b90f4cad5153e
Zach <147861361+1054009@users.noreply.github.com>
2024-04-18 14:54:43 -0400

    Test connection with FreeDB

--- src/server.py
+++ src/server.py
@@ -1,10 +1,24 @@
+# Imports
+import platform
 from flask import Flask, render_template
+from sqlalchemy import create_engine, engine_from_config, text
 
+# FreeDB information
+FREEDB_USERNAME = "freedb_1054009"
+FREEDB_PASSWORD = "$hchYjtYar$BAh2" # pls don't hack kthx
+FREEDB_HOST = "sql.freedb.tech"
+FREEDB_PORT = 3306
+
+# Setup Flask and connect to the database
 app = Flask(__name__)
+engine = create_engine(f"mysql://{FREEDB_USERNAME}:{FREEDB_PASSWORD}@{FREEDB_HOST}:{FREEDB_PORT}")
+sql = engine.connect()
 
+# Routes
 @app.route("/")
 def test():
     return render_template("base.html")
 
-# if __name__ == "__main__":
-# 	app.run()
+# Brap you
+if platform.system() == "Windows" and __name__ == "__main__":
+ 	app.run()
