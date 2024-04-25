093dd1838f16283b3c5e01837e51d99166b699cf
Zach <147861361+1054009@users.noreply.github.com>
2024-04-25 00:09:40 -0400

    Route favicon

---
+++ src/scripts/routes/favicon.py
@@ -0,0 +1,15 @@
+from flask import send_file, url_for
+from pathlib import Path
+
+@app.route("/favicon.ico")
+def favicon():
+	favicon_path = Path(
+		app.static_folder + "/.." +
+
+		url_for(
+			"static",
+			filename = "img/favicon.ico"
+		)
+	).resolve()
+
+	return send_file(favicon_path)
