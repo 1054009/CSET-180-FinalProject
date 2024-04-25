9ea45c173fcdfe5f406fccee32de15af5716ccad
Zach <147861361+1054009@users.noreply.github.com>
2024-04-24 23:41:06 -0400

    Dynamic routing

---
+++ src/server/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from pathlib import Path
+from secrets import token_hex
+import os
+
+EXECUTING_DIRECTORY = Path(__file__).parent.resolve()
+
+app = Flask(
+	__name__,
+
+	static_folder = (EXECUTING_DIRECTORY / "../client/static"),
+	template_folder = (EXECUTING_DIRECTORY / "../client/templates")
+)
+
+app.secret_key = token_hex()
+
+# Load routes
+for route_name in os.listdir(EXECUTING_DIRECTORY / "../scripts/routes"):
+	if not route_name.endswith(".py"):
+		continue
+
+	route_path = (EXECUTING_DIRECTORY / f"../scripts/routes/{route_name}").resolve()
+
+	try:
+		exec(open(route_path).read(), globals())
+	except Exception as error:
+		print(error)
--- src/server/server.py
+++ src/server/server.py
@@ -1,25 +1,13 @@
 # Imports
-from flask import Flask
-from pathlib import Path
 from platform import system
-from secrets import token_hex
 
+from app import app
 from engine import engine
 from models import Base
 from session import session
 
 # Setup database
 Base.metadata.create_all(bind = engine)
-
-# Initialize Flask
-SERVER_DIRECTORY = Path(__file__).parent.resolve()
-
-app = Flask(
-	__name__,
-	template_folder = (SERVER_DIRECTORY / "../templates")
-)
-
-app.secret_key = token_hex()
 
 if system() == "Windows" and __name__ == "__main__":
 	app.run()
