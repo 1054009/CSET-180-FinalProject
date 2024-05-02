f7681090b0599d6da28aea9f00673539ee2494ff
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 11:57:34 -0400

    Add stats and styling to home page

--- src/server/routes/home.py
+++ src/server/routes/home.py
@@ -1,7 +1,19 @@
 from app import app
 from flask import render_template
+from database_session import database_session
+from models import User, Product, Review
 
 @app.route("/")
 @app.route("/home/")
 def home():
-	return render_template("home.html")
+	user_count = database_session.query(User).count()
+	product_count = database_session.query(Product).count()
+	review_count = database_session.query(Review).count()
+
+	return render_template(
+		"home.html",
+
+		user_count = user_count,
+		product_count = product_count,
+		review_count = review_count
+	)
