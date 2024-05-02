d8c68b407a849cd105d70f45fc2da79a48bc069c
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 13:20:16 -0400

    Create custom render_template to always pass through session

---
+++ src/server/template_renderer.py
@@ -0,0 +1,13 @@
+import flask
+
+def render_template(template_name, **data):
+	return flask.render_template(
+		template_name,
+
+		session = {
+			"user_id": flask.session.get("user_id"),
+			"email_address": flask.session.get("email_address")
+		},
+
+		**data
+	)
