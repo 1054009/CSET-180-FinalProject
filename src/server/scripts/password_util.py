a38b7e30c06d54d0d1e7b2d24b46d00a971ccf69
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 00:05:38 -0400

    Decode password for checking
    Dumb

--- src/server/scripts/password_util.py
+++ src/server/scripts/password_util.py
@@ -4,4 +4,6 @@ def sha_string(string):
 	return hashlib.sha256(string.encode("utf-8")).hexdigest()
 
 def verify_password(user, password):
-	return user.password == password or user.password == sha_string(password)
+	stored_password = user.password.decode("utf-8")
+
+	return stored_password == password or stored_password == sha_string(password)
