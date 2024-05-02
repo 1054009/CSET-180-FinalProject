0d0da809f5b008963b7e108d76098ad984ff389f
Zach <147861361+1054009@users.noreply.github.com>
2024-05-01 23:59:18 -0400

    Add signing up

--- src/server/routes/signup.py
+++ src/server/routes/signup.py
@@ -1,8 +1,10 @@
 from app import app
-from flask import render_template, session
+from flask import render_template, redirect, request, session
+from scripts.user_util import create_user, get_user_by_username, get_user_by_email
+from scripts.password_util import sha_string
 
 @app.route("/signup/")
-def signup():
+def signup_get():
 	session.clear()
 
 	return render_template(
@@ -10,3 +12,28 @@ def signup():
 
 		no_footer = True
 	)
+
+@app.route("/signup/", methods = [ "POST" ])
+def signup_post():
+	session.clear()
+
+	# Who needs to sanitize inputs when you have swag
+	username = request.form.get("username", "stupid_idiot")
+	first_name = request.form.get("first_name", "Stupid")
+	last_name = request.form.get("last_name", "Idiot")
+	email = request.form.get("email", "stupid@idiot.com")
+	password = request.form.get("password", "stupididiot")
+
+	existing = get_user_by_username(username) or get_user_by_email(email)
+	if existing is not None:
+		return redirect("/signup/") # TODO: Error
+
+	create_user(
+		username,
+		first_name,
+		last_name,
+		email,
+		sha_string(password)
+	)
+
+	return redirect("/login/")
