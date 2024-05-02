e73107473fc9099091e830080997001dbd3326b4
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 14:27:24 -0400

    Sort product imports

--- src/server/routes/products.py
+++ src/server/routes/products.py
@@ -1,8 +1,8 @@
 from app import app
 from database_session import database_session
 from models import Product, ProductImage, ProductDiscount
-from template_renderer import render_template
 from scripts.object_util import objects_as_json
+from template_renderer import render_template
 
 @app.route("/products/")
 def products():
