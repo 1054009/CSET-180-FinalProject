fd2661cc1be723da193fec2e3359b0791e0c8c97
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 15:10:02 -0400

    Passthrough product_images
    Holy moly

--- src/server/routes/products.py
+++ src/server/routes/products.py
@@ -7,9 +7,11 @@ from template_renderer import render_template
 @app.route("/products/")
 def products():
 	product_list = objects_as_json(Product)
+	product_images = objects_as_json(ProductImage)
 
 	return render_template(
 		"products.html",
 
-		product_list = product_list
+		product_list = product_list,
+		product_images = product_images
 	)
