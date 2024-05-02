4035506d776702bce335fb880323df0fdeb17004
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 00:00:45 -0400

    It's password not hashed_password idiot

--- src/server/scripts/password_util.py
+++ src/server/scripts/password_util.py
@@ -4,4 +4,4 @@ def sha_string(string):
 	return hashlib.sha256(string.encode("utf-8")).hexdigest()
 
 def verify_password(user, password):
-	return user.hashed_password == password or user.hashed_password == sha_string(password)
+	return user.password == password or user.password == sha_string(password)
