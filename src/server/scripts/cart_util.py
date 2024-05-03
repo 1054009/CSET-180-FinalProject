03ca064bc9469a16ae8d7a6ff3c24360aba34423
Zach <147861361+1054009@users.noreply.github.com>
2024-05-03 12:36:37 -0400

    Create get_usable_carts

---
+++ src/server/scripts/cart_util.py
@@ -0,0 +1,14 @@
+from database_session import database_session
+from models import Product, ProductImage, ProductDiscount, Cart, Order
+from scripts.user_util import get_user_by_email
+
+def get_usable_carts(email_address):
+	user = get_user_by_email(email_address)
+	if user is None:
+		return []
+
+	return database_session.query(Cart)									\
+							.outerjoin(Order, Cart.id == Order.cart_id)		\
+							.filter(Order.cart_id is None)					\
+							.order_by(Cart.id.desc())						\
+							.all()
