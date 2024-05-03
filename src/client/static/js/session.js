9cf918a35d6bd07e014b1737e8eccd1ca19bbf4d
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 23:32:22 -0400

    That didn't work for some reason
    Stupid helper class oh wait I made it

--- src/client/static/js/session.js
+++ src/client/static/js/session.js
@@ -5,7 +5,7 @@ const g_Helper = new Helper()
 g_Helper.hookEvent(window, "load", false, () =>
 {
 	document.body.g_SessionData = SESSION // Make sure everyone can access it
-	document.body.g_bSessionIsValid = g_Helper.getBoolean(g_SessionData)
+	document.body.g_bSessionIsValid = SESSION.email_address != null
 
 	// TODO: Edit navbar
 	console.log(SESSION)
