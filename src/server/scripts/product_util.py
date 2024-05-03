cc0b08fad1fd673470724ee9601869d974dad332
Zach <147861361+1054009@users.noreply.github.com>
2024-05-03 14:12:33 -0400

    Add get_products

--- src/server/scripts/product_util.py
+++ src/server/scripts/product_util.py
@@ -1,6 +1,9 @@
 from database_session import database_session
 from models import Product
 
+def get_products():
+	return database_session.query(Product).all()
+
 def get_product(id):
 	try:
 		products = database_session.query(Product)
