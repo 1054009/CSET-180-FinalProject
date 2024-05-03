a834ee24a81602d11cf0d340f59882aef12e5325
Zach <147861361+1054009@users.noreply.github.com>
2024-05-03 13:20:23 -0400

    Create validate_session

--- src/server/routes/products.py
+++ src/server/routes/products.py
@@ -1,10 +1,9 @@
 from app import app
-from database_session import database_session
 from flask import request, redirect, session
 from models import Product, ProductImage, ProductDiscount
 from scripts.cart_util import add_to_cart
 from scripts.object_util import objects_as_json
-from scripts.user_util import get_user_by_email
+from scripts.session_util import validate_session
 from template_renderer import render_template
 
 @app.route("/products/")
@@ -21,12 +20,12 @@ def products_get():
 
 @app.route("/products/add_to_cart/", methods = [ "POST" ])
 def products_post():
-	product_id = request.form.get("product_id")
-	if product_id is None:
+	user = validate_session(session)
+	if not user:
 		return redirect("/products/") # TODO: Error
 
-	user = get_user_by_email(session.get("email_address"))
-	if user is None:
+	product_id = request.form.get("product_id")
+	if product_id is None:
 		return redirect("/products/") # TODO: Error
 
 	success = add_to_cart(user.email_address, product_id)
---
+++ src/server/scripts/session_util.py
@@ -0,0 +1,9 @@
+from scripts.user_util import get_user_by_email
+
+def validate_session(flask_session):
+	email_user = get_user_by_email(flask_session.get("email_address"))
+
+	if email_user.id == flask_session.get("user_id"):
+		return email_user
+	else:
+		return False
