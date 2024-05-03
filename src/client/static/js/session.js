0cc0d0b04f23b0b10b24e2e7f7430c8a968ec630
Zach <147861361+1054009@users.noreply.github.com>
2024-05-02 23:31:09 -0400

    Add g_bSessionIsValid

--- src/client/static/js/session.js
+++ src/client/static/js/session.js
@@ -5,6 +5,8 @@ const g_Helper = new Helper()
 g_Helper.hookEvent(window, "load", false, () =>
 {
 	document.body.g_SessionData = SESSION // Make sure everyone can access it
+	document.body.g_bSessionIsValid = g_Helper.getBoolean(g_SessionData)
+
 	// TODO: Edit navbar
 	console.log(SESSION)
 })
