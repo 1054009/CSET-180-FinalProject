2d2e710a7ff3ba19e01a7d7836cb3cf49e99d040
Zach <147861361+1054009@users.noreply.github.com>
2024-05-03 12:47:02 -0400

    Create get_product

---
+++ src/server/scripts/product_util.py
@@ -0,0 +1,10 @@
+from database_session import database_session
+from models import Product
+
+def get_product(id):
+	try:
+		products = database_session.query(Product)
+
+		return products.filter(Product.id == id).first()
+	except:
+		return None
