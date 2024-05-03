f076ae86517fd41c41d7aef59b0a0f140219fa57
Zach <147861361+1054009@users.noreply.github.com>
2024-05-03 14:58:04 -0400

    Begin cart handling on client

--- src/client/static/js/cart.js
+++ src/client/static/js/cart.js
@@ -8,9 +8,39 @@ const g_Builder = new DOMBuilder()
 
 var g_ItemList = []
 
+function updateCartList()
+{
+	const cart_grid = document.querySelector("#cart_grid")
+	if (!g_Helper.isValidElement(cart_grid)) return // TODO: Error
+
+	g_Builder.start(cart_grid)
+	{
+		for (const item of g_ItemList)
+		{
+			g_Builder.startElement("div")
+			{
+				g_Builder.addClass("flexbox")
+				g_Builder.addClass("flex_vcenter")
+				g_Builder.addClass("flex_hspace")
+				g_Builder.addClass("flex_fill")
+
+				g_Builder.startElement("div")
+				{
+					g_Builder.setProperty("innerHTML", item.product_id)
+				}
+				g_Builder.endElement()
+			}
+			g_Builder.endElement()
+		}
+	}
+	g_Builder.end()
+}
+
 g_Helper.hookEvent(window, "load", false, () =>
 {
 	g_ItemList = fixJSONList(CART_ITEMS)
 
 	console.log(g_ItemList)
+
+	updateCartList()
 })
