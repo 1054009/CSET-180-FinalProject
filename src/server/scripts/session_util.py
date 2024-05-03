372f9636d6db1756dee92c705b0b82181fc783cd
Zach <147861361+1054009@users.noreply.github.com>
2024-05-03 13:25:42 -0400

    Validate before testing in validate_session

--- src/server/scripts/session_util.py
+++ src/server/scripts/session_util.py
@@ -2,6 +2,8 @@ from scripts.user_util import get_user_by_email
 
 def validate_session(flask_session):
 	email_user = get_user_by_email(flask_session.get("email_address"))
+	if email_user is None:
+		return False
 
 	if email_user.id == flask_session.get("user_id"):
 		return email_user
