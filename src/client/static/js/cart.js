d117f3fce414131a88c3543c28055c42423c2b81
Zach <147861361+1054009@users.noreply.github.com>
2024-05-04 14:24:24 -0400

    Start cart layout and styling

--- src/client/static/js/cart.js
+++ src/client/static/js/cart.js
@@ -17,16 +17,81 @@ function updateCartList()
 	{
 		for (const item of g_ItemList)
 		{
+			const productData = item.product
+			if (!productData) continue // TODO: Error
+
+			productData.images = fixJSONList(productData.images)
+
 			g_Builder.startElement("div")
 			{
-				g_Builder.addClass("flexbox")
-				g_Builder.addClass("flex_vcenter")
-				g_Builder.addClass("flex_hspace")
-				g_Builder.addClass("flex_fill")
+				g_Builder.addClass("upper_glow")
 
 				g_Builder.startElement("div")
 				{
-					g_Builder.setProperty("innerHTML", item.product_id)
+					g_Builder.addClass("cart_item")
+					g_Builder.addClass("flexbox")
+					g_Builder.addClass("flex_vcenter")
+					g_Builder.addClass("flex_hspace")
+					g_Builder.addClass("flex_fill")
+
+					g_Builder.startElement("div")
+					{
+						g_Builder.addClass("flexbox")
+						g_Builder.addClass("flex_vcenter")
+						g_Builder.addClass("flex_gap")
+						g_Builder.addClass("flex_fill")
+
+						// TODO: Image background
+						// TODO: Fix when no image
+						const imageObject = productData.images[0]
+						if (imageObject)
+						{
+							g_Builder.startElement("img")
+							{
+								g_Builder.setProperty("src", imageObject.image_data)
+							}
+							g_Builder.endElement()
+						}
+
+						g_Builder.startElement("h4")
+						{
+							g_Builder.setProperty("innerHTML", productData.name)
+						}
+						g_Builder.endElement()
+					}
+					g_Builder.endElement()
+
+					g_Builder.startElement("div")
+					{
+						g_Builder.addClass("flexbox")
+						g_Builder.addClass("flex_vcenter")
+						g_Builder.addClass("flex_fill")
+
+						g_Builder.startElement("p")
+						{
+							g_Builder.setProperty("innerHTML", `${item.quantity} in cart`)
+						}
+						g_Builder.endElement()
+					}
+					g_Builder.endElement()
+
+					g_Builder.startElement("form")
+					{
+						g_Builder.addClass("flexbox")
+						g_Builder.addClass("flex_vcenter")
+						g_Builder.addClass("flex_fill")
+
+						g_Builder.startElement("input")
+						{
+							g_Builder.addClass("float_right")
+
+							g_Builder.setAttribute("type", "button")
+
+							g_Builder.setProperty("value", "Remove")
+						}
+						g_Builder.endElement()
+					}
+					g_Builder.endElement()
 				}
 				g_Builder.endElement()
 			}
@@ -40,7 +105,9 @@ g_Helper.hookEvent(window, "load", false, () =>
 {
 	g_ItemList = fixJSONList(CART_ITEMS)
 
-	console.log(g_ItemList)
+
 
 	updateCartList()
+
+	console.log(g_ItemList)
 })
