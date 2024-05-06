dcc3a768fb90a8713aa737aa478126d257342aa0
Zach <147861361+1054009@users.noreply.github.com>
2024-05-06 13:06:23 -0400

    Add more time utility functions

--- src/server/scripts/time_util.py
+++ src/server/scripts/time_util.py
@@ -1,4 +1,13 @@
 from datetime import datetime
 
+def get_current_time():
+	return datetime.now()
+
+def time_to_timestamp(time):
+	return time.strftime("%Y-%m-%d %H:%M:%S")
+
 def get_current_timestamp():
-	return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+	return time_to_timestamp(get_current_time())
+
+def timestamp_to_time(timestamp):
+	return datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
