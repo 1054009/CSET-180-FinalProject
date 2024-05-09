9cc97e395b45d823e050a6a18eeb527d91c4631c
Zach <147861361+1054009@users.noreply.github.com>
2024-05-09 14:28:47 -0400

    Stop dropage
    Hopefully it's done

--- src/server/server.py
+++ src/server/server.py
@@ -7,9 +7,9 @@ from engine import engine
 from models import Base
 
 # Setup database
-Base.metadata.reflect(engine)
-Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+# Base.metadata.reflect(engine)
+# Base.metadata.drop_all(engine)
+# Base.metadata.create_all(engine)
 
 if system() == "Windows" and __name__ == "__main__":
 	app.run()
