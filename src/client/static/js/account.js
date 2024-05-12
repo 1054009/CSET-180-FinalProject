fa20f8b9f3957e3e7528271def4e1c7f6d5c2864
Zach <147861361+1054009@users.noreply.github.com>
2024-05-11 20:09:43 -0400

    Maybe just one

--- src/client/static/js/account.js
+++ src/client/static/js/account.js
@@ -9,8 +9,6 @@ g_Helper.hookEvent(window, "load", false, () =>
 	const control_div = document.querySelector("#controls > div")
 	if (!g_Helper.isValidElement(control_div)) return // TODO: Error
 
-	console.log(document.body.g_SessionData)
-
 	g_Builder.start(control_div)
 	{
 		switch (document.body.g_SessionData.user_type)
@@ -24,25 +22,11 @@ g_Helper.hookEvent(window, "load", false, () =>
 			{
 				g_Builder.startElement("input")
 				{
+					g_Builder.addClass("flex_fill")
+
 					g_Builder.setAttribute("type", "button")
 
-					g_Builder.setProperty("value", "Add Product")
-				}
-				g_Builder.endElement()
-
-				g_Builder.startElement("input")
-				{
-					g_Builder.setAttribute("type", "button")
-
-					g_Builder.setProperty("value", "Delete Product")
-				}
-				g_Builder.endElement()
-
-				g_Builder.startElement("input")
-				{
-					g_Builder.setAttribute("type", "button")
-
-					g_Builder.setProperty("value", "Edit Product")
+					g_Builder.setProperty("value", "Edit Products")
 				}
 				g_Builder.endElement()
 
